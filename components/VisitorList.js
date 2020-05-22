b014efc3df0e555b730c5e118d162c0171d2eb4b
Adnan48 <36703285+Adnan48@users.noreply.github.com>
2020-05-22 14:45:09 +0530

    Updated VisitorList

--- components/VisitorList.js
+++ components/VisitorList.js
@@ -3,46 +3,52 @@ import {View, Text, FlatList} from 'react-native';
 import {connect} from 'react-redux';
 import Header from './Header';
 
-class VisitorList extends PureComponent{
+class VisitorList extends PureComponent {
+  renderFlatListItem = ({item, index}) => (
+    <ListItem
+      name={item.name}
+      email={item.email}
+      personToVisit={item.personToVisit}
+    />
+  );
 
-    renderFlatListItem = ({item, index}) => (
-            <ListItem name={item.name} email={item.email} personToVisit={item.personToVisit} />
-        )
-    
-
-    render(){
-        return(
-            <View>
-                <Header title="Visitor List" onPressOpen={() => this.props.navigation.openDrawer() } />
-                <FlatList
-                data={this.props.userList}
-                renderItem={this.renderFlatListItem}
-                keyExtractor={(item, index) => `${index}` }
-                />
-                </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Header
+          title="Visitor List"
+          onPressOpen={() => this.props.navigation.openDrawer()}
+        />
+        <FlatList
+          data={this.props.userList}
+          renderItem={this.renderFlatListItem}
+          keyExtractor={(item, index) => `${index}`}
+        />
+      </View>
+    );
+  }
 }
 
-
 function mapStateToProps(reduxStore) {
-    return {
-      userList: reduxStore.uiReducer.userList,
-    };
-  }
-  
-  export default connect(mapStateToProps, null)(VisitorList);
+  return {
+    userList: reduxStore.uiReducer.userList,
+  };
+}
 
-  class ListItem extends PureComponent{
-      render(){
-          const {name, email, personToVisit} = this.props;
-          return(
-              <View style={{ margin: 5, padding: 10, borderWidth: 1, borderRadius: 5 }} >
-                    <Text>Name: {name}</Text>
-                  <Text>Email: {email}</Text>
-                  <Text>Person To Visit: {personToVisit} </Text>
+export default connect(
+  mapStateToProps,
+  null,
+)(VisitorList);
 
-              </View>
-          )
-      }
+class ListItem extends PureComponent {
+  render() {
+    const {name, email, personToVisit} = this.props;
+    return (
+      <View style={{margin: 5, padding: 10, borderWidth: 1, borderRadius: 5}}>
+        <Text>Name: {name}</Text>
+        <Text>Email: {email}</Text>
+        <Text>Person To Visit: {personToVisit} </Text>
+      </View>
+    );
   }
+}
